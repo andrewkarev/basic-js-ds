7a75d7442384a32600915152441613fad66de620
andrewkarev <an.carev4@gmail.com>
2022-03-12 17:52:15 +0300

    feat: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,136 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { Node } = require('../extensions/list-tree.js');
 
-// const { Node } = require('../extensions/list-tree.js');
-
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this._root = insertElememnt(this._root, data);
+
+    function insertElememnt(node, value) {
+      if (!node) {
+        return new Node(value);
+      }
+
+      if (value === node.data) {
+        return node;
+      }
+
+      if (value < node.data) {
+        node.left = insertElememnt(node.left, value);
+      } else {
+        node.right = insertElememnt(node.right, value);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let currentNode = this._root;
+
+    while (currentNode) {
+      if (data === currentNode.data) {
+        return true;
+      }
+
+      if (data < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+
+      if (!currentNode) {
+        return false;
+      }
+    }
+
+    return false
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return nodeSearch(this._root, data);
+
+    function nodeSearch(node, value) {
+      if (!node) return null;
+      if (node.data === value) return node;
+      if (node.data > data) {
+        return nodeSearch(node.left, value);
+      } else {
+        return nodeSearch(node.right, value);
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this._root = removeElement(this._root, data);
+
+    function removeElement(node, value) {
+      if (!node) {
+        return null;
+      }
+
+      if (value === node.data) {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+        if (node.left && node.right) {
+          let substitute = node.right;
+
+          while (substitute.left) {
+            substitute = substitute.left;
+          }
+
+          node.data = substitute.data;
+          node.right = removeElement(node.right, substitute.data);
+          return node;
+        }
+      } else if (value < node.data) {
+        node.left = removeElement(node.left, value);
+      } else {
+        node.right = removeElement(node.right, value);
+      }
+      return node;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentNode = this._root;
+
+    while (currentNode) {
+      if (!currentNode.left) {
+        return currentNode.data;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentNode = this._root;
+
+    while (currentNode) {
+      if (!currentNode.right) {
+        return currentNode.data;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+
   }
 }
 
